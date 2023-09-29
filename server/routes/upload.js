c5383c6c0f500e38173abf77a48da4b150ab2b6c
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-09-29 13:36:45 +0100

    feat: added feature

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -2,17 +2,19 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const hbjs = require("handbrake-js");
-const upload = multer({ dest: "../uploads" });
-const path = require("path");
-const moveAndRename = require("./moveRenameFile");
+const upload = multer({ dest: "../compressedUploads" });
+const moveAndRename = require("./fileHandler");
 //Endpoint to receive uploads
 //multiple res.writes with each having a key to access.
 router.get("/", async (req, res) => {});
 router.post("/", upload.single("video"), async (req, res) => {
   try {
     if (!req.file) {
-      return res.status(400).write({ messagemulter: "No file uploaded" });
+      return res.status(400).send({ messagemulter: "No file uploaded" });
     }
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Connection", "keep-alive");
+
     //get file and upload to uploads
     const input = req.file.path;
     //for compressedUploads
@@ -31,7 +33,7 @@ router.post("/", upload.single("video"), async (req, res) => {
         res.status(500).json({ messagehbjs: "Compression failed" });
       })
       .on("progress", (progress) => {
-        console.log(
+        res.write(
           "Percent complete: %d, ETA: %d",
           progress.percentComplete,
           progress.eta
@@ -39,9 +41,10 @@ router.post("/", upload.single("video"), async (req, res) => {
       })
       .on("complete", () => {
         console.log("Compression complete!");
-        res.status(200).send("Operation complete");
-        moveAndRename(originalname);
+        res.status(200).write("Operation complete");
+        moveAndRename(originalname, input);
       });
+    res.end();
   } catch (err) {
     console.log(err);
   }
