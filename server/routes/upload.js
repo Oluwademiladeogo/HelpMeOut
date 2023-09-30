b569a3a9006ce9f3a7497ebd6d298bdebdba14bf
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-09-30 13:34:15 +0100

    Unuse handbrake

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -27,38 +27,35 @@ router.post("/", upload.single("video"), async (req, res) => {
     const originalname = req.file.originalname;
     transcribe(input, audioFile, originalname);
 
-    const options = {
-      input: input,
-      output: "compressedvideo",
-      encoder: "x264",
-      quality: 20,
-    };
-    //handbrake for compression
-    try {
-      await hbjs
-        .spawn(options)
-        .on("error", (err) => {
-          console.error(err.message);
-          res
-            .status(500)
-            .json({ hbjsError: "Compression failed please retry" });
-        })
-        .on("progress", (progress) => {
-          console.log(
-            "Percent complete: %d, ETA: %d",
-            progress.percentComplete,
-            progress.eta
-          );
-        })
-        .on("complete", () => {
-          console.log("Compression complete!");
-          // res.status(200).send("Operation complete");
-          moveAndRename(originalname, input);
-          res.redirect("/videos?name=" + originalname);
-        });
-    } catch (ex) {
-      console.log;
-    }
+    // const options = {
+    //   input: input,
+    //   output: "compressedvideo",
+    //   encoder: "x264",
+    //   quality: 20,
+    // };
+    // //handbrake for compression
+    // try {
+    //   await hbjs
+    //     .spawn(options)
+    //     .on("error", (err) => {
+    //       console.error(err.message);
+    //       res
+    //         .status(500)
+    //         .json({ hbjsError: "Compression failed please retry" });
+    //     })
+    //     .on("progress", (progress) => {
+    //       console.log(
+    //         "Percent complete: %d, ETA: %d",
+    //         progress.percentComplete,
+    //         progress.eta
+    //       );
+    //     })
+    //     .on("complete", () => {
+    //       console.log("Compression complete!");
+    //       // res.status(200).send("Operation complete");
+    //       moveAndRename(originalname, input);
+    res.redirect("/videos?name=" + originalname);
+    // });
   } catch (err) {
     console.log(err);
   }
